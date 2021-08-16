fc863a3ec231ee1bf10d271476b903eab0441e52
Pete Baston <pete@petebaston.com>
2021-08-16 12:23:27 +0100

    Update

--- src/components/Datepicker/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import dynamic from "next/dynamic"
-export default function index(props) {
-  const DatepickerComponent = dynamic(() => import("./DatepickerComponent"), {
-    ssr: false,
-  })
-  return <DatepickerComponent {...props} />
-}
